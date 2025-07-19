cae2c89cfd6b7d04551b08ee76ba52f179861b8a
ravelojhon <jhonjairoravelomora@gmail.com>
2025-07-19 16:31:48 -0500

    ajustes microfronend

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,76 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { fileURLToPath } from 'url';
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default (env, argv) => {
-  const isStandalone = process.env.STANDALONE === 'true';
-  
+  const isStandalone = process.env.STANDALONE === "true";
+
   return {
-    entry: isStandalone ? './client/src/standalone.tsx' : './client/src/main.tsx', // Punto de entrada adaptado
+    entry: isStandalone
+      ? "./client/src/standalone.tsx"
+      : "./client/src/main.tsx", // Punto de entrada adaptado
     output: {
-      filename: 'erp360-auth.js', // Nombre del microfrontend
-      libraryTarget: isStandalone ? 'var' : 'system', // system para single-spa, var para standalone
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: 'http://localhost:8083/', // Puerto único para este microfrontend
+      filename: "sass-auth.js", // Nombre del microfrontend
+      libraryTarget: isStandalone ? "var" : "system", // system para single-spa, var para standalone
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "http://localhost:8083/", // Puerto único para este microfrontend
     },
-    mode: 'development',
+    mode: "development",
     devServer: {
       port: 8083, // Puerto diferente al de tu sidebar
       headers: {
-        'Access-Control-Allow-Origin': '*', // CORS para el root-config
+        "Access-Control-Allow-Origin": "*", // CORS para el root-config
       },
       historyApiFallback: true,
       hot: true,
       static: {
-        directory: path.join(__dirname, 'client'),
+        directory: path.join(__dirname, "client"),
       },
     },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript'
-            ]
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader'
-        ],
-      },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          use: ["@svgr/webpack"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./client/index.html",
+        filename: "index.html",
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './client/index.html',
-      filename: 'index.html',
-    }),
-  ],
-    externals: isStandalone ? [] : ['react', 'react-dom', 'single-spa', 'single-spa-react'], // Dependencias compartidas solo en modo single-spa
+    externals: isStandalone
+      ? []
+      : ["react", "react-dom", "single-spa", "single-spa-react"], // Dependencias compartidas solo en modo single-spa
     resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
       alias: {
-        '@': path.resolve(__dirname, 'client/src'),
-        '@shared': path.resolve(__dirname, 'shared'),
-        '@assets': path.resolve(__dirname, 'attached_assets'),
+        "@": path.resolve(__dirname, "client/src"),
+        "@shared": path.resolve(__dirname, "shared"),
+        "@assets": path.resolve(__dirname, "attached_assets"),
       },
     },
   };
